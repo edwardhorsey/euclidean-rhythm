03c8302cf7bbaba4176b6bc7ee8f4d9a701c1285
Edward Horsey <edwardhorsey@protonmail.com>
2025-01-16 23:22:26 +0000

    Add metadata, analytics

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
